9cf48f2d244414b9efcaac18907b1d206b94cdf7
Yingxiangempire <yingxiangempire@outlook.com>
2016-09-04 10:50:21 +0800

    PHPinfo

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxiangwang
+ * Date: 16/9/3
+ * Time: 下午2:51
+ */
+
+namespace App\Http\Controllers;
+
+
+class AuthController extends Controller
+{
+    public function getToken()
+    {
+        return \Request::input("echostr");
+    }
+}
