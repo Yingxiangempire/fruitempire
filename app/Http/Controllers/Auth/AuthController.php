f18ec63132f97114bba64199242786843ad00679
Yingxiangempire <yingxiangempire@outlook.com>
2016-09-05 23:50:49 +0800

    草

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -90,8 +90,8 @@ class AuthController extends Controller
         $wechat = app('wechat');
         $message = new Text(['content' => '你有下级代理商了,牛逼啊!']);
         $user_id=Request::all();
-        dump($user_id);
-       // $result = $wechat->staff->message($message)->to($user_id->id)->send();
+
+        $result = $wechat->staff->message($message)->to($user_id['driver'])->send();
 //...
 
 
