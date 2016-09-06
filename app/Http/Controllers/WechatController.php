e052e86823d4a553ecff8c25f790eb9d8002913e
Yingxiangempire <yingxiangempire@outlook.com>
2016-09-06 20:33:56 +0800

    wangye

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,8 +17,8 @@ class WechatController extends Controller
 {
 
     const QR_PREFIX="qr_";
-    const WEIXIN_URL_PART1="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1e8f4b777c70344b&redirect_uri=http%3A%2F%2Fwww.yingxiangempire.com%2Fauth%2Fcallback%3Fdriver%3D";
-    const WEIXIN_URL_PART2="&response_type=code&scope=snsapi_userinfo&state=FekCuCqFwYos7ro39nQWSngtVhOPhxQbe56ariOS&connect_redirect=1#wechat_redirect";
+    const WEIXIN_URL_PART1="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdde3cbe598010374&redirect_uri=http%3A%2F%2Fwww.yingxiangempire.com%2Fauth%2Fcallback%3Fdriver%3D";
+    const WEIXIN_URL_PART2="&response_type=code&scope=snsapi_userinfo&state=zbo8czE7ZkXrKJHppublVT0GHVI8xPUduPjtYoFS&connect_redirect=1#wechat_redirect";
     public $wechat;
     public $base_path;
     public function __construct()
