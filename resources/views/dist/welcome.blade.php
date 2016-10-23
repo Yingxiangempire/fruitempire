806bfbcae222f00f43b8aadc5d9445e16a602684
Yingxiangempire <yingxiangempire@outlook.com>
2016-10-23 16:02:18 +0800

    build

--- resources/views/dist/welcome.blade.php
+++ resources/views/dist/welcome.blade.php
@@ -1,46 +1,46 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Laravel</title>
+    <head>
+        <title>Laravel</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-    <style>
-        html, body {
-            height: 100%;
-        }
+        <style>
+            html, body {
+                height: 100%;
+            }
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
 
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
 
-        .title {
-            font-size: 54px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title"><h6>点击下方图片,识别二维码添加关注</h6></div>
-        <img src="/image/860.jpg" class="img-circle avatar"  alt="User Image">
-    </div>
-</div>
-</body>
+            .title {
+                font-size: 54px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title"><h6>点击下方图片,识别二维码添加关注</h6></div>
+                <img src="/image/258.jpg" class="img-circle avatar"  alt="User Image">
+            </div>
+        </div>
+    </body>
 </html>
