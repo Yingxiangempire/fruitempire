e4102a3993eff6908805d18c6893c8be6e4fdfc6
Yingxiangempire <yingxiangempire@outlook.com>
2016-11-09 21:44:58 +0800

    notice

--- app/Logic/Wechat/LNotice.php
+++ app/Logic/Wechat/LNotice.php
@@ -7,25 +7,45 @@ namespace App\Logic\Wechat;
  * Date: 16/9/12
  * Time: 下午3:54
  */
-class LGroup
+class LNotice
 {
     public $wechat;
-    public $group;
+    public $notice;
 
     public function __construct()
     {
         $this->wechat = app('wechat');
-        $this->group = $this->wechat->user_group;
+        $this->notice = $this->wechat->notice;
     }
 
-    public function setGroup($name)
+    public function setIndustry($industryId1, $industryId2)
     {
-        return $this->group->create($name);
+        return $this->notice->setIndustry($industryId1, $industryId2);
     }
 
-    public function setGroupUser($group_id, $openId)
+    public function getIndustry()
     {
-       return $this->group->moveUser($openId, $group_id);
+       return $this->notice->getIndustry();
+    }
+
+    public function addTemplate()
+    {
+        return $this->notice->addTemplate();
+    }
+
+    public function send($message)
+    {
+        return $this->notice->send($message);
+    }
+
+    public function getPrivateTemplates()
+    {
+        return $this->notice->getPrivateTemplates();
+    }
+
+    public function deletePrivateTemplate($templateId)
+    {
+        return $this->notice->deletePrivateTemplate($templateId);
     }
 
 
